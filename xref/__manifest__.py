09e06cfa7ebae9f3204216836eb0aeaedaf50d99
pmn <mn.pouya@gmail.com>
2019-01-15 01:59:28 +0330

    [add] manifest and class diagrams

---
+++ xref/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "xref",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+    'qweb':[
+        'static/xml/templates.xml',
+    ],
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/xref_views.xml',
+        'views/ir_model_views.xml',
+        'views/assets.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
